86cb6c7f37c0ada34c1e0a4b2f6b059b686ac1e9
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-03-03 19:16:28 +0700

    added input text and button as well as changed the styling of header

--- app/src/main/java/com/fairuzsakhiy/todo/MainActivity.kt
+++ app/src/main/java/com/fairuzsakhiy/todo/MainActivity.kt
@@ -10,14 +10,22 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.googlefonts.GoogleFont
@@ -51,19 +59,46 @@ val provider = GoogleFont.Provider(
     providerPackage = "com.google.android.gms",
     certificates = R.array.com_google_android_gms_fonts_certs
 )
-val fontName = GoogleFont("Poppins")
+val fontName = GoogleFont("Inter")
 val fontFamily = FontFamily(
     Font(googleFont = fontName, fontProvider = provider)
 )
 
 @Composable
 fun ToDoApps() {
+    var inputValue by remember {
+        mutableStateOf("")
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFFeeeeee))
+
     ) {
         Header()
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+
+        ) {
+            OutlinedTextField(
+                value = inputValue,
+                onValueChange = { inputValue = it },
+                label = { Text("enter something in mind...")},
+                textStyle = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold, fontFamily = fontFamily,),
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Button(onClick = {  },
+                modifier = Modifier
+                    .align(Alignment.End)
+            ) {
+                Text("Add")
+            }
+        }
+
     }
 }
 
@@ -75,7 +110,7 @@ fun Header(modifier: Modifier = Modifier) {
             .fillMaxWidth()
             .padding(16.dp)
         ,
-        horizontalArrangement = Arrangement.Center
+        horizontalArrangement = Arrangement.Start
     ) {
         Text(
             text = stringResource(id = R.string.header),
