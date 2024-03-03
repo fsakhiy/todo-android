294908e1ba7570ca984248623d5c2dcde21454ed
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-03-03 18:38:03 +0700

    added header

--- app/src/main/java/com/fairuzsakhiy/todo/MainActivity.kt
+++ app/src/main/java/com/fairuzsakhiy/todo/MainActivity.kt
@@ -3,14 +3,31 @@ package com.fairuzsakhiy.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.fairuzsakhiy.todo.ui.theme.ToDoTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +39,59 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ToDoApps()
                 }
             }
         }
     }
 }
 
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+val fontName = GoogleFont("Poppins")
+val fontFamily = FontFamily(
+    Font(googleFont = fontName, fontProvider = provider)
+)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ToDoApps() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFeeeeee))
+    ) {
+        Header()
+    }
+}
+
+@Composable
+fun Header(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .background(Color(0xFF40064a))
+            .fillMaxWidth()
+            .padding(16.dp)
+        ,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(id = R.string.header),
+            fontSize = 24.sp,
+            fontFamily = fontFamily,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ToDoTheme {
-        Greeting("Android")
+        ToDoApps()
     }
 }
